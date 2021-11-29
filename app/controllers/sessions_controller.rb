2ef10b0dbf50e91cbe7c7aecfcaa2190bbc2a1fc
houzairkoussa <houzairmk@icloud.com>
2021-11-29 23:26:45 +0400

    added index create to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+  def index
+    @sessions = Session.all
+  end
+
+  def create
+    @session = Session.new(session_params)
+    @game = Game.find(params[:game_id])
+    @session.game = @game
+    @session.user = current_user
+    @session.save!
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:score, :game_id, :user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :games, only: %i[show index]
+  resources :games, only: %i[index] do
+    resources :sessions, only: %i[index create]
+  end
 end
