c7f76042a43dae7a7ec7fe29d604a2efc1bf56e6
houzairkoussa <houzairmk@icloud.com>
2021-12-01 02:13:45 +0400

    user can create new session if signed in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,23 +4,16 @@ class SessionsController < ApplicationController
   end
 
   def new
-    @session = Session.new
-    @game = Game.find(params[:game_id])
-    @user = current_user
-    render "games/#{@game.title}"
+    create
   end
 
   def create
-    @session = Session.new(session_params)
+    @session = Session.new
     @game = Game.find(params[:game_id])
+    @user = current_user
+    @session.user = @user
     @session.game = @game
-    @session.user = current_user
     @session.save!
-  end
-
-  private
-
-  def session_params
-    params.require(:session).permit(:score, :game_id, :user_id)
+    redirect_to "/#{@game.title}"
   end
 end
