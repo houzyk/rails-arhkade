e9c5c5fa43fafc06d0c126b7a611233c80a7bc24
houzairkoussa <houzairmk@icloud.com>
2021-11-30 09:29:48 +0400

    generated board

--- app/javascript/controllers/checkers_controller.js
+++ app/javascript/controllers/checkers_controller.js
@@ -1,9 +1,20 @@
 import { Controller } from "stimulus";
 
+const checkersGame = (game) => {
+  let colour;
+  let start = -1;
+  const colourAssign = (index) => colour = (index % 2 === 0) ? "b" : "w";
+  for (let index = 0; index < 64; index++) {
+    if (index % 8 === 0) start += 1;
+    (start % 2 === 0) ? colourAssign(index) : colourAssign(index - 1);
+    game.insertAdjacentHTML('beforeend', `<div class="grid-checkers grid-checkers-${colour}"></div>`);
+  }
+}
 export default class extends Controller {
-  static targets = []
+  static targets = ["game"]
 
   connect() {
     console.log("Checkers!");
+    checkersGame(this.gameTarget);
   }
 }
